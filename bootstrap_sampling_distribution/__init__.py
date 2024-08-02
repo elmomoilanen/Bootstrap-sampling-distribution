7b8f586d3db75b259b4972bb3d0bff8fc1ba2c18
Elmo Moilanen <49366097+elmomoilanen@users.noreply.github.com>
2024-08-02 18:01:59 +0300

    chore: use explicit exported names for imports in __init__.py

--- bootstrap_sampling_distribution/__init__.py
+++ bootstrap_sampling_distribution/__init__.py
@@ -15,23 +15,23 @@ For convenience some common statistical functions have been implemented library
 compatibly in the `statistics` module.
 """
 
-from bootstrap_sampling_distribution.errors import SampDistError
-from bootstrap_sampling_distribution.sampling import SampDist
+from bootstrap_sampling_distribution.errors import SampDistError as SampDistError
+from bootstrap_sampling_distribution.sampling import SampDist as SampDist
 
 from bootstrap_sampling_distribution.statistics import (
-    mean,
-    geometric_mean,
-    harmonic_mean,
-    trimmed_mean_factory,
-    median,
-    maximum,
-    minimum,
-    skew,
-    kurtosis,
-    interquartile_range,
-    standard_deviation,
-    median_absolute_deviation,
-    quantile_factory,
-    corr_pearson,
-    corr_spearman,
+    mean as mean,
+    geometric_mean as geometric_mean,
+    harmonic_mean as harmonic_mean,
+    trimmed_mean_factory as trimmed_mean_factory,
+    median as median,
+    maximum as maximum,
+    minimum as minimum,
+    skew as skew,
+    kurtosis as kurtosis,
+    interquartile_range as interquartile_range,
+    standard_deviation as standard_deviation,
+    median_absolute_deviation as median_absolute_deviation,
+    quantile_factory as quantile_factory,
+    corr_pearson as corr_pearson,
+    corr_spearman as corr_spearman,
 )
